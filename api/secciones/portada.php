df61c600b3c3048798595aaf738f79bcf40028ba
jho-k <129234250+jho-k@users.noreply.github.com>
2025-02-19 05:10:10 -0800

    Add files via upload

---
+++ api/secciones/portada.php
@@ -0,0 +1,11 @@
+<?php
+/*****
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+*****/
+
+$portada = [
+"titulo" => "Diario El Hocicón, "Pobre, pero Honrado"",
+"autor" => "XXXX",
+"resumen" => "El resumen Febrero 2025",
+];
+?>
