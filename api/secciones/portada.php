2f88052a64c8f84a570fdd035ee745317793f61b
jho-k <129234250+jho-k@users.noreply.github.com>
2025-02-19 05:12:58 -0800

    Update portada.php

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -4,7 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "Diario El Hocicón, "Pobre, pero Honrado"",
+"titulo" => "Diario El Hocicón", "Pobre, pero Honrado",
 "autor" => "XXXX",
 "resumen" => "El resumen Febrero 2025",
 ];
